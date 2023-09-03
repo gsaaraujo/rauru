caece6aeb789cc2c9c998c036782bdd4a2123ae9
Gabriel <gabriel.houth@gmail.com>
2023-09-02 22:34:19 -0300

    feat: add error when provided data is in the past

--- rauru/src/domain/models/time-slot.ts
+++ rauru/src/domain/models/time-slot.ts
@@ -1,4 +1,8 @@
+import { BaseError } from '@shared/helpers/base-error';
 import { ValueObject } from '@shared/helpers/value-object';
+import { Either, left, right } from '@shared/helpers/either';
+
+import { TimeSlotCannotBeInThePastError } from '@domain/errors/time-slot-cannot-be-in-the-past-error';
 
 export enum TimeSlotStatus {
   AVAILABLE = 'AVAILABLE',
@@ -11,8 +15,16 @@ export type TimeSlotProps = {
 };
 
 export class TimeSlot extends ValueObject<TimeSlotProps> {
-  static create(props: Omit<TimeSlotProps, 'status'>): TimeSlot {
-    return new TimeSlot({ date: props.date, status: TimeSlotStatus.AVAILABLE });
+  static create(props: Omit<TimeSlotProps, 'status'>): Either<BaseError, TimeSlot> {
+    const currentDate = new Date();
+
+    if (props.date.getTime() < currentDate.getTime()) {
+      const error = new TimeSlotCannotBeInThePastError('The time slot cannot be in the past.');
+      return left(error);
+    }
+
+    const timeSlot = new TimeSlot({ date: props.date, status: TimeSlotStatus.AVAILABLE });
+    return right(timeSlot);
   }
 
   static reconstitute(props: TimeSlotProps): TimeSlot {
--- rauru/src/domain/models/time-slot.unit.spec.ts
+++ rauru/src/domain/models/time-slot.unit.spec.ts
@@ -1,18 +1,42 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+
+import { Either } from '@shared/helpers/either';
+import { BaseError } from '@shared/helpers/base-error';
 
 import { TimeSlot, TimeSlotStatus } from '@domain/models/time-slot';
+import { TimeSlotCannotBeInThePastError } from '@domain/errors/time-slot-cannot-be-in-the-past-error';
 
 describe('time-slot', () => {
+  beforeEach(() => {
+    vi.useRealTimers();
+  });
+
   it('should create timeSlot', () => {
+    const currentDate = new Date('2023-08-20T10:00:00Z');
+    vi.setSystemTime(currentDate);
     const output: TimeSlot = TimeSlot.reconstitute({
       date: new Date('2023-08-20T14:00:00Z'),
       status: TimeSlotStatus.AVAILABLE,
     });
 
-    const sut: TimeSlot = TimeSlot.create({
+    const sut: Either<BaseError, TimeSlot> = TimeSlot.create({
       date: new Date('2023-08-20T14:00:00Z'),
     });
 
-    expect(sut).toStrictEqual(output);
+    expect(sut.isRight()).toBeTruthy();
+    expect(sut.value).toStrictEqual(output);
+  });
+
+  it('should fail if the provided date is less than the current date', () => {
+    const currentDate = new Date('2023-08-20T14:00:00Z');
+    vi.setSystemTime(currentDate);
+    const output = new TimeSlotCannotBeInThePastError('The time slot cannot be in the past.');
+
+    const sut: Either<BaseError, TimeSlot> = TimeSlot.create({
+      date: new Date('2023-08-18T08:00:00Z'),
+    });
+
+    expect(sut.isLeft()).toBeTruthy();
+    expect(sut.value).toStrictEqual(output);
   });
 });
