2380919a89a7ca8d8b578924d7ea5c2e8665c257
Gabriel <gabriel.houth@gmail.com>
2023-09-03 10:49:04 -0300

    feat: add error case for when appointment is being booked in the past

--- rauru/src/domain/models/appointment/appointment.ts
+++ rauru/src/domain/models/appointment/appointment.ts
@@ -1,6 +1,8 @@
 import { Entity } from '@shared/helpers/entity';
-import { Either, right } from '@shared/helpers/either';
 import { BaseError } from '@shared/helpers/base-error';
+import { Either, left, right } from '@shared/helpers/either';
+
+import { AppointmentCannotBeInThePastError } from '@domain/errors/appointment-cannot-be-in-the-past-error';
 
 export type AppointmentProps = {
   doctorId: string;
@@ -10,6 +12,13 @@ export type AppointmentProps = {
 
 export class Appointment extends Entity<AppointmentProps> {
   public static create(props: AppointmentProps): Either<BaseError, Appointment> {
+    const currentDate = new Date();
+
+    if (props.date.getTime() < currentDate.getTime()) {
+      const error = new AppointmentCannotBeInThePastError('Cannot book an appointment in the past.');
+      return left(error);
+    }
+
     const appointment = new Appointment(props);
     return right(appointment);
   }
--- rauru/src/domain/models/appointment/appointment.unit.spec.ts
+++ rauru/src/domain/models/appointment/appointment.unit.spec.ts
@@ -4,14 +4,30 @@ import { Either } from '@shared/helpers/either';
 import { BaseError } from '@shared/helpers/base-error';
 
 import { Appointment } from '@domain/models/appointment/appointment';
+import { AppointmentCannotBeInThePastError } from '@domain/errors/appointment-cannot-be-in-the-past-error';
 
 describe('appointment', () => {
   it('should create Appointment', () => {
     const output: Appointment = Appointment.reconstitute({
       doctorId: 'any',
       patientId: 'any',
-      date: new Date('2020-08-18T08:00:00Z'),
+      date: new Date('2100-08-18T08:00:00Z'),
     });
+
+    const sut: Either<BaseError, Appointment> = Appointment.create({
+      doctorId: 'any',
+      patientId: 'any',
+      date: new Date('2100-08-18T08:00:00Z'),
+    });
+
+    expect(sut.isRight()).toBeTruthy();
+    expect((sut.value as Appointment).doctorId).toBe(output.doctorId);
+    expect((sut.value as Appointment).patientId).toBe(output.patientId);
+    expect((sut.value as Appointment).date).toStrictEqual(output.date);
+  });
+
+  it('should fail if date is less than current date', () => {
+    const output = new AppointmentCannotBeInThePastError('Cannot book an appointment in the past.');
 
     const sut: Either<BaseError, Appointment> = Appointment.create({
       doctorId: 'any',
@@ -19,9 +35,7 @@ describe('appointment', () => {
       date: new Date('2020-08-18T08:00:00Z'),
     });
 
-    expect(sut.isRight()).toBeTruthy();
-    expect((sut.value as Appointment).doctorId).toBe(output.doctorId);
-    expect((sut.value as Appointment).patientId).toBe(output.patientId);
-    expect((sut.value as Appointment).date).toStrictEqual(output.date);
+    expect(sut.isLeft()).toBeTruthy();
+    expect(sut.value).toStrictEqual(output);
   });
 });
